bb0a185a361426707e3907e48dc8267a2dc4e60c
devkingel <devkingel@gmail.com>
2024-09-01 11:52:13 +0200

    Borders shadow and responsive improvement

--- frontend/dev/blogHeader.ts
+++ frontend/dev/blogHeader.ts
@@ -6,8 +6,35 @@ class BlogHeader extends LitElement {
   @property({type: Boolean})
   menuOpen = true;
 
+  /** Sizes  */
+  @property({type: Number})
+  smallWidth = 600;
+
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+
+  @property({type: Number}) windowWidth: number = window.innerWidth;
+
+  private updateWindowSize() {
+    this.windowWidth = window.innerWidth;
+
+    /** Close the hamburger menu if it is not shown */
+    if (this.windowWidth >= this.smallWidth) {
+      this.menuOpen = false;
+    }
+  }
+
+  override connectedCallback() {
+    super.connectedCallback();
+    // Ajouter un écouteur d'événements pour détecter la redimension de la fenêtre
+    window.addEventListener('resize', this.updateWindowSize.bind(this));
+  }
+
+  override disconnectedCallback() {
+    super.disconnectedCallback();
+    // Retirer l'écouteur d'événements lorsque le composant est retiré du DOM
+    window.removeEventListener('resize', this.updateWindowSize.bind(this));
   }
 
   static override styles = css`
@@ -15,10 +42,10 @@ class BlogHeader extends LitElement {
       display: flex;
       align-items: center;
       justify-content: space-between;
-      padding: 0 20px 0 15px;
+      padding: 0 0px 0px 0px;
       box-shadow: 0px 1px 10px #acacac;
     }
-    
+
     .navbar-container-open {
       flex-direction: column;
       align-items: start;
@@ -27,6 +54,7 @@ class BlogHeader extends LitElement {
     .logo a {
       text-decoration: none;
       color: #3d3d3d;
+      margin-left: 20px;
     }
 
     .hamburger-lines {
@@ -39,6 +67,7 @@ class BlogHeader extends LitElement {
       border: 1px solid #949494;
       box-shadow: 1px 1px 3px #acacac;
       border-radius: 3px;
+      margin-right: 20px;
     }
 
     .hamburger-lines:hover {
@@ -61,6 +90,7 @@ class BlogHeader extends LitElement {
       position: absolute;
       top: 23px;
       right: 20px;
+      margin-right: 0px;
     }
 
     .active .hamburger-line1 {
@@ -75,16 +105,16 @@ class BlogHeader extends LitElement {
       transform: rotate(-45deg) translate(5px, -5px);
     }
 
-    .menu-items-hide {
+    .menu-items {
       display: none;
     }
 
     .menu-items-show {
+      display: block;
       width: 100%;
-    }
-
-    .menu-items-show > menu {
       flex-direction: column;
+      box-shadow: rgba(0, 0, 0, 0.05) 0px -2px 2px 0px;
+      padding-left: 0px;
     }
 
     .menu-items-show li {
@@ -94,11 +124,7 @@ class BlogHeader extends LitElement {
       box-shadow: rgba(0, 0, 0, 0.05) 0px 2px 2px 0px;
     }
 
-    .menu-items-show menu {
-      padding-left: 0px;
-    }
-
-    .menu-items-show menu:first-child {
+    .menu-items-show:first-child {
       margin-top: 0px;
     }
 
@@ -106,35 +132,63 @@ class BlogHeader extends LitElement {
       text-decoration: none;
       color: #3d3d3d;
     }
+
+    @media screen and (min-width: 600px) {
+      .hamburger-lines {
+        display: none;
+      }
+
+      .menu-items {
+        display: flex;
+        align-self: stretch;
+        padding-left: 0px;
+        margin: 0px 0px;
+        box-shadow: rgba(0, 0, 0, 0.05) -2px 0px 2px 0px;
+      }
+
+      .menu-items > li {
+        display: flex;
+        list-style-type: none;
+        border: none;
+        box-shadow: rgba(0, 0, 0, 0.05) 2px 0px 2px 0px;
+        padding: 0px 12px;
+        align-items: center;
+      }
+
+      .menu-items li > a {
+        text-decoration: none;
+        color: #3d3d3d;
+      }
+    }
   `;
 
   override render() {
     return html`
       <header>
-        <nav class="navbar">
-          <div class="container ${this.menuOpen ? 'navbar-container-open' : ''}">
-            <div class="logo">
-              <h1><a href="#">Mon Blog</a></h1>
-            </div>
-            <div
-              class="hamburger-lines 
-              ${this.menuOpen ? 'active' : ''}"
-              @click="${this.toggleMenu}"
-            >
-              <span class="hamburger-line hamburger-line1"></span>
-              <span class="hamburger-line hamburger-line2"></span>
-              <span class="hamburger-line hamburger-line3"></span>
-            </div>
-            <div class="menu-items ${this.menuOpen ? 'menu-items-show' : 'menu-items-hide'}">
-              <menu>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">blogs</a></li>
-              <li><a href="#">portfolio</a></li>
-              <li><a href="#">about</a></li>
-              <li><a href="#">contact</a></li>
-              </menu>
-            </div>
+        <nav
+          class="navbar container ${this.menuOpen
+            ? 'navbar-container-open'
+            : ''}"
+        >
+          <div class="logo">
+            <h1><a href="#">Mon Blog</a></h1>
           </div>
+          <div
+            class="hamburger-lines 
+              ${this.menuOpen ? 'active' : ''}"
+            @click="${this.toggleMenu}"
+          >
+            <span class="hamburger-line hamburger-line1"></span>
+            <span class="hamburger-line hamburger-line2"></span>
+            <span class="hamburger-line hamburger-line3"></span>
+          </div>
+          <menu class="menu-items ${this.menuOpen ? 'menu-items-show' : ''}">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">blogs</a></li>
+            <li><a href="#">portfolio</a></li>
+            <li><a href="#">about</a></li>
+            <li><a href="#">contact</a></li>
+          </menu>
         </nav>
       </header>
     `;
--- frontend/src/blogHeader.js
+++ frontend/src/blogHeader.js
@@ -10,19 +10,39 @@ let BlogHeader = class BlogHeader extends LitElement {
     constructor() {
         super(...arguments);
         this.menuOpen = true;
+        /** Sizes  */
+        this.smallWidth = 600;
+        this.windowWidth = window.innerWidth;
     }
     toggleMenu() {
         this.menuOpen = !this.menuOpen;
+    }
+    updateWindowSize() {
+        this.windowWidth = window.innerWidth;
+        /** Close the hamburger menu if it is not shown */
+        if (this.windowWidth >= this.smallWidth) {
+            this.menuOpen = false;
+        }
+    }
+    connectedCallback() {
+        super.connectedCallback();
+        // Ajouter un écouteur d'événements pour détecter la redimension de la fenêtre
+        window.addEventListener('resize', this.updateWindowSize.bind(this));
+    }
+    disconnectedCallback() {
+        super.disconnectedCallback();
+        // Retirer l'écouteur d'événements lorsque le composant est retiré du DOM
+        window.removeEventListener('resize', this.updateWindowSize.bind(this));
     }
     static { this.styles = css `
     .container {
       display: flex;
       align-items: center;
       justify-content: space-between;
-      padding: 0 20px 0 15px;
+      padding: 0 0px 0px 0px;
       box-shadow: 0px 1px 10px #acacac;
     }
-    
+
     .navbar-container-open {
       flex-direction: column;
       align-items: start;
@@ -31,6 +51,7 @@ let BlogHeader = class BlogHeader extends LitElement {
     .logo a {
       text-decoration: none;
       color: #3d3d3d;
+      margin-left: 20px;
     }
 
     .hamburger-lines {
@@ -43,6 +64,7 @@ let BlogHeader = class BlogHeader extends LitElement {
       border: 1px solid #949494;
       box-shadow: 1px 1px 3px #acacac;
       border-radius: 3px;
+      margin-right: 20px;
     }
 
     .hamburger-lines:hover {
@@ -65,6 +87,7 @@ let BlogHeader = class BlogHeader extends LitElement {
       position: absolute;
       top: 23px;
       right: 20px;
+      margin-right: 0px;
     }
 
     .active .hamburger-line1 {
@@ -79,16 +102,16 @@ let BlogHeader = class BlogHeader extends LitElement {
       transform: rotate(-45deg) translate(5px, -5px);
     }
 
-    .menu-items-hide {
+    .menu-items {
       display: none;
     }
 
     .menu-items-show {
+      display: block;
       width: 100%;
-    }
-
-    .menu-items-show > menu {
       flex-direction: column;
+      box-shadow: rgba(0, 0, 0, 0.05) 0px -2px 2px 0px;
+      padding-left: 0px;
     }
 
     .menu-items-show li {
@@ -98,11 +121,7 @@ let BlogHeader = class BlogHeader extends LitElement {
       box-shadow: rgba(0, 0, 0, 0.05) 0px 2px 2px 0px;
     }
 
-    .menu-items-show menu {
-      padding-left: 0px;
-    }
-
-    .menu-items-show menu:first-child {
+    .menu-items-show:first-child {
       margin-top: 0px;
     }
 
@@ -110,34 +129,62 @@ let BlogHeader = class BlogHeader extends LitElement {
       text-decoration: none;
       color: #3d3d3d;
     }
+
+    @media screen and (min-width: 600px) {
+      .hamburger-lines {
+        display: none;
+      }
+
+      .menu-items {
+        display: flex;
+        align-self: stretch;
+        padding-left: 0px;
+        margin: 0px 0px;
+        box-shadow: rgba(0, 0, 0, 0.05) -2px 0px 2px 0px;
+      }
+
+      .menu-items > li {
+        display: flex;
+        list-style-type: none;
+        border: none;
+        box-shadow: rgba(0, 0, 0, 0.05) 2px 0px 2px 0px;
+        padding: 0px 12px;
+        align-items: center;
+      }
+
+      .menu-items li > a {
+        text-decoration: none;
+        color: #3d3d3d;
+      }
+    }
   `; }
     render() {
         return html `
       <header>
-        <nav class="navbar">
-          <div class="container ${this.menuOpen ? 'navbar-container-open' : ''}">
-            <div class="logo">
-              <h1><a href="#">Mon Blog</a></h1>
-            </div>
-            <div
-              class="hamburger-lines 
-              ${this.menuOpen ? 'active' : ''}"
-              @click="${this.toggleMenu}"
-            >
-              <span class="hamburger-line hamburger-line1"></span>
-              <span class="hamburger-line hamburger-line2"></span>
-              <span class="hamburger-line hamburger-line3"></span>
-            </div>
-            <div class="menu-items ${this.menuOpen ? 'menu-items-show' : 'menu-items-hide'}">
-              <menu>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">blogs</a></li>
-              <li><a href="#">portfolio</a></li>
-              <li><a href="#">about</a></li>
-              <li><a href="#">contact</a></li>
-              </menu>
-            </div>
+        <nav
+          class="navbar container ${this.menuOpen
+            ? 'navbar-container-open'
+            : ''}"
+        >
+          <div class="logo">
+            <h1><a href="#">Mon Blog</a></h1>
           </div>
+          <div
+            class="hamburger-lines 
+              ${this.menuOpen ? 'active' : ''}"
+            @click="${this.toggleMenu}"
+          >
+            <span class="hamburger-line hamburger-line1"></span>
+            <span class="hamburger-line hamburger-line2"></span>
+            <span class="hamburger-line hamburger-line3"></span>
+          </div>
+          <menu class="menu-items ${this.menuOpen ? 'menu-items-show' : ''}">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">blogs</a></li>
+            <li><a href="#">portfolio</a></li>
+            <li><a href="#">about</a></li>
+            <li><a href="#">contact</a></li>
+          </menu>
         </nav>
       </header>
     `;
@@ -146,6 +193,12 @@ let BlogHeader = class BlogHeader extends LitElement {
 __decorate([
     property({ type: Boolean })
 ], BlogHeader.prototype, "menuOpen", void 0);
+__decorate([
+    property({ type: Number })
+], BlogHeader.prototype, "smallWidth", void 0);
+__decorate([
+    property({ type: Number })
+], BlogHeader.prototype, "windowWidth", void 0);
 BlogHeader = __decorate([
     customElement('blog-header')
 ], BlogHeader);
