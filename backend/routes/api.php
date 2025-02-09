d4204e2c0524d5a92512a6b29735ff2149d22912
devkingel <devkingel@gmail.com>
2025-02-09 10:20:25 +0100

    models and migrations creation for the blog database

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\PostController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
+Route::post('/posts', [PostController::class, 'store'])->middleware('auth:sanctum');
