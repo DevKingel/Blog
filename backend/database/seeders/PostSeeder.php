240eb2d90c0ef4427285f5ce1773c7a202703c3a
devkingel <devkingel@gmail.com>
2025-02-09 21:13:41 +0100

    Create seeders

---
+++ backend/database/seeders/PostSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\Tag;
+use App\Models\Comment;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Générer 50 catégories
+        Category::factory(50)->create();
+
+        // Générer 200 tags
+        Tag::factory(200)->create();
+
+        // Générer 3000 posts
+        Post::factory(3000)->create()->each(function ($post) {
+            // Associer 1 à 3 tags aléatoires
+            $tags = Tag::inRandomOrder()->limit(rand(1, 3))->pluck('id');
+            $post->tags()->attach($tags);
+
+            // Générer 0 à 5 commentaires par post
+            Comment::factory(rand(0, 5))->create(['post_id' => $post->id]);
+        });
+    }
+}
