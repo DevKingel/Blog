7728e7fa0865350e328e5e89e13174b3fdeac446
devkingel <devkingel@gmail.com>
2025-02-15 08:51:51 +0100

    Add new routes for the blog API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,11 +5,66 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\PostController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
+Route::prefix('/user')->name('user.')->group(function () {
+    Route::get('/', [PostController::class, 'index'])->middleware('auth:sanctum')->name('all');
 
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/{post}', [PostController::class, 'show']);
-Route::post('/posts', [PostController::class, 'store'])->middleware('auth:sanctum');
+    Route::get('/{id}', [PostController::class, 'show'])->middleware('auth:sanctum')->name('one');
+
+    Route::post('/add', [PostController::class, 'store'])->middleware('auth:sanctum')->name('add');
+    
+    Route::delete('/delete/{id}', [PostController::class, 'delete'])->middleware('auth:sanctum')->name('delete');
+
+    Route::patch('/update/{id}', [PostController::class, 'update'])->middleware('auth:sanctum')->name('update');
+});
+
+Route::prefix('/posts')->name('post.')->group(function () {
+    Route::get('/', [PostController::class, 'index'])->name('all');
+
+    Route::get('/{slug}-{id}', [PostController::class, 'show'])->name('one');
+
+    Route::post('/add', [PostController::class, 'store'])->middleware('auth:sanctum')->name('add');
+    
+    Route::delete('/delete/{slug}-{id}', [PostController::class, 'delete'])->middleware('auth:sanctum')->name('delete');
+
+    Route::patch('/update/{slug}-{id}', [PostController::class, 'update'])->middleware('auth:sanctum')->name('update');
+});
+
+Route::prefix('/tags')->name('tag.')->group(function () {
+    Route::get('/', [PostController::class, 'index'])->name('all');
+
+    Route::get('/{slug}-{id}', [PostController::class, 'show'])->name('one');
+
+    Route::post('/add', [PostController::class, 'store'])->middleware('auth:sanctum')->name('add');
+    
+    Route::delete('/delete/{slug}-{id}', [PostController::class, 'delete'])->middleware('auth:sanctum')->name('delete');
+
+    Route::patch('/update/{slug}-{id}', [PostController::class, 'update'])->middleware('auth:sanctum')->name('update');
+});
+
+Route::prefix('/comments')->name('comment.')->group(function () {
+    Route::get('/', [PostController::class, 'index'])->name('all');
+
+    Route::get('/{id}', [PostController::class, 'show'])->name('one');
+
+    Route::post('/add', [PostController::class, 'store'])->middleware('auth:sanctum')->name('add');
+    
+    Route::delete('/delete/{id}', [PostController::class, 'delete'])->middleware('auth:sanctum')->name('delete');
+
+    Route::patch('/update/{id}', [PostController::class, 'update'])->middleware('auth:sanctum')->name('update');
+});
+
+Route::prefix('/categories')->name('catagory.')->group(function () {
+    Route::get('/', [PostController::class, 'index'])->name('all');
+
+    Route::get('/{slug}-{id}', [PostController::class, 'show'])->name('one');
+
+    Route::post('/add', [PostController::class, 'store'])->middleware('auth:sanctum')->name('add');
+    
+    Route::delete('/delete/{slug}-{id}', [PostController::class, 'delete'])->middleware('auth:sanctum')->name('delete');
+
+    Route::patch('/update/{slug}-{id}', [PostController::class, 'update'])->middleware('auth:sanctum')->name('update');
+});
