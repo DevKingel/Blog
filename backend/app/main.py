67a62f29ecc7b4df7b1f360ee49942b630373b76
devkingel <devkingel@gmail.com>
2025-08-05 20:21:15 +0200

    updated main

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,19 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from app.api.v1.endpoints import category, post, tag, users
+from app.api.v1.endpoints import (
+    admin,
+    auth,
+    categories,
+    comments,
+    media,
+    posts,
+    profile,
+    search,
+    stats,
+    tags,
+    users,
+)
 from app.db.session import engine
 from app.models.user import User  # Import your models
 
@@ -14,7 +26,8 @@ from .core.config import settings
 async def lifespan(app: FastAPI):
     # On startup, create database tables
     async with engine.begin() as conn:
-        # await conn.run_sync(User.metadata.drop_all) # Use this to drop tables for a fresh start
+        # await conn.run_sync(User.metadata.drop_all)
+        # # Use this to drop tables for a fresh start
         await conn.run_sync(User.metadata.create_all)
     yield
     # On shutdown, you can add cleanup code here
@@ -25,17 +38,36 @@ app = FastAPI(
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
     lifespan=lifespan,
 )
-app.add_middleware(APIKeyMiddleware, api_key=settings.API_KEY)
+app.add_middleware(APIKeyMiddleware, api_key=settings.SECRET_KEY)
 
 # Include the user router
 app.include_router(users.router, prefix=f"{settings.API_V1_STR}/users", tags=["users"])
 
-# Include the post, category, and tag routers
-app.include_router(post.router, prefix=f"{settings.API_V1_STR}", tags=["posts"])
+app.include_router(auth.router, prefix=f"{settings.API_V1_STR}/auth", tags=["auth"])
 app.include_router(
-    category.router, prefix=f"{settings.API_V1_STR}", tags=["categories"]
+    profile.router, prefix=f"{settings.API_V1_STR}/profile", tags=["profile"]
 )
-app.include_router(tag.router, prefix=f"{settings.API_V1_STR}", tags=["tags"])
+
+# Include the post, category, tag, and comment routers
+app.include_router(posts.router, prefix=f"{settings.API_V1_STR}", tags=["posts"])
+app.include_router(
+    categories.router, prefix=f"{settings.API_V1_STR}", tags=["categories"]
+)
+app.include_router(tags.router, prefix=f"{settings.API_V1_STR}", tags=["tags"])
+
+
+app.include_router(
+    comments.router, prefix=f"{settings.API_V1_STR}/comments", tags=["comments"]
+)
+
+app.include_router(stats.router, prefix=f"{settings.API_V1_STR}/stats", tags=["stats"])
+
+app.include_router(
+    search.router, prefix=f"{settings.API_V1_STR}/search", tags=["search"]
+)
+
+app.include_router(media.router, prefix=f"{settings.API_V1_STR}/media", tags=["media"])
+app.include_router(admin.router, prefix=f"{settings.API_V1_STR}/admin", tags=["admin"])
 
 
 @app.get("/")
